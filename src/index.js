4b5d1ac23824d843f0564064792c0d5f78565a67
gkphiri <166555047+gkphiri@users.noreply.github.com>
2024-04-19 23:52:28 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -6,24 +6,51 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = newDate(response.data.tme * 1000);
+  let date = new Date(response.data.tme * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidty}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  timeElement.innerHTML = FormDate(date);
+  timeElement.innerHTML = FormatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"/>`;
+}
+function FormatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}${hours}:${minutes}`;
 }
 
-function search(event) {
+function searchCity(city) {
   let apiKey = "d5b8b088efba45fe37c3af7t975co9d8";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
+  let searchFormInput = document.querySelector("#search-form-input");
 }
 let searchInput = document.querySelector("#search-form");
 searchInput.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
