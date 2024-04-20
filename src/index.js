5213e1fc5f7614be0d060a82fbdb4ce74dce9b78
gkphiri <166555047+gkphiri@users.noreply.github.com>
2024-04-20 18:01:32 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
